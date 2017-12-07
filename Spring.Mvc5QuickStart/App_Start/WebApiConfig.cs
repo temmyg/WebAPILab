3202ef0832945c48ee2e6f91bc442e63fe29bbe7
DESKTOP-7QVTD5J\ZHG <hugzg@hotmail.com>
2017-12-07 11:16:49 -0500

    First version

---
+++ Spring.Mvc5QuickStart/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Spring.Mvc5QuickStart.Filters;
+
+namespace Spring.Mvc5QuickStart
+{
+
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Filters.Add(new CustomExceptionFilterAttribute());
+            
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute("controller", "api/{controller}/{action}", new { action = RouteParameter.Optional  });
+        }
+    }
+}
---
+++ Spring.Mvc5QuickStart/Spring.Mvc5QuickStart/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Net.Http;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Spring.Mvc5QuickStart.Filters
+{
+    public class CustomExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            actionExecutedContext.Response = new HttpResponseMessage()
+            {
+                Content = new StringContent("not intended invocation"),
+                StatusCode = HttpStatusCode.BadRequest
+            };
+        }
+
+        public override Task OnExceptionAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                actionExecutedContext.Response = new HttpResponseMessage()
+                {
+                    Content = new StringContent("not intended invocation"),
+                    StatusCode = HttpStatusCode.BadRequest
+                };
+            });
+        }
+    }
+}
